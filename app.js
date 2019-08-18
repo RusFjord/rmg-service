7ce40b4b3362dc1ff558dcad2f8fc5a9e75a197d
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2019-08-18 22:12:04 +0300

    Добавление mongoose в зависимости.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
+
 
 app.listen(5555, () => {
 	console.log('Сервер работает на порту 5555');
