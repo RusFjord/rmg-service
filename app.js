1c36ca91c9c81ec46ff9227ebe03d038c542e758
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2019-08-19 01:44:46 +0300

    Добавлены роуты 404 и sendmail

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const sendmail = require('./routes/sendmail');
+const page404 = require('./routes/page404');
+
 const app = express();
 app.use(express.json());
 
+app.use('/api_v1', sendmail);
+
+app.use(page404);
 
 app.listen(5555, () => {
 	console.log('Сервер работает на порту 5555');
