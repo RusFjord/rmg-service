b1546e45a35ad2ecabfa0811b9e5b18ed084d208
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2019-08-20 06:23:37 +0300

    Конфигурация модуля nodemailer

--- routes/sendmail.js
+++ routes/sendmail.js
@@ -1,9 +1,36 @@
 const router = require('express').Router();
+const nodeMailer = require('nodemailer');
 
 router.post('/sendmail', (req, res) => {
     const data = req.body;
     console.log(data);
-    res.send('Works!');
+    let text = `Получено обращение с сайта\rИмя: ${data.name}\rТелефон: ${data.phone}\rЭл. почта: ${data.email}\rОрганизация: ${data.organization}`;
+
+    let transporter = nodeMailer.createTransport({
+        host: 'smtp.yandex.ru',
+        port: 465,
+        secure: true,
+        auth: {
+            login: 'teocharmillion@yandex.ru',
+            pass: 'Fyfcnfcbz.2012'
+        }
+    });
+
+    let mailOption = {
+        from:'teocharmillion@yandex.ru',
+        to: 'f.endovitskiy@ulteam8.ru',
+        subject: 'Запрос с сайта',
+        text: text
+    };
+
+    transporter.sendMail(mailOption, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send(`Ошибка: ${error}`);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+        res.send('Письмо отправлено!');
+        });
 });
 
 router.get('/sendmail', (req, res) => {
